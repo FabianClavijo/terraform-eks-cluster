a729e3a248a5c050e668d68df419b6367c18210a
Gauss Magno <broly2022@hotmail.com>
2025-09-14 07:49:19 -0500

    fixing deprecated sufix

--- main.tf
+++ main.tf
@@ -173,7 +173,7 @@ resource "aws_subnet" "aks2" {
 ############################
 
 resource "aws_eip" "nat" {
-  vpc = true
+  domain = "vpc"
 
   tags = merge(var.tags, { Name = "${var.name_prefix}-nat-eip" })
 }
